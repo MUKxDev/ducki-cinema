4d2f72429600496d617bf0373372bcce25b122d5
Mohamed Mukhtar <mukxdev@gmail.com>
2022-09-12 17:30:48 +0300

    added dashboard page

--- src/pages/StartUp.tsx
+++ src/pages/StartUp.tsx
@@ -1,5 +1,6 @@
 import Auth from "../components/Auth";
 import { useAppContext } from "../context/appContext";
+import Dashboard from "./Dashboard";
 // import Account from "./Account";
 import DuckiPlayer from "./DuckiPlayer";
 
@@ -11,11 +12,12 @@ export default function StartUp() {
         <Auth />
       ) : (
         //   <Account key={currentSession.user.id}
-        <DuckiPlayer
-          url="https://s186.vidboo.org/6jmn257lqmazsalrixoqb25rkckpcv7uag7uixqzqjb7trtrech5jmvkvxaq/v.mp4"
-          // url="https://s196.vidboo.org/6jmnvot2vmazsalriuoqhypmiteb7avnuzzk5abnpuou3ghgvhtvxayxnkyq/v.mp4"
-          // url="https://ajye.vizcloud.ink/simple/EqPFIfwQWADtjDlGha7rC5ouqVwVvra2AgR7rqk+wYMnU94US2El/br/list.m3u8"
-        ></DuckiPlayer>
+        <Dashboard></Dashboard>
+        // <DuckiPlayer
+        //   url="https://s186.vidboo.org/6jmn257lqmazsalrixoqb25rkckpcv7uag7uixqzqjb7trtrech5jmvkvxaq/v.mp4"
+        //   // url="https://s196.vidboo.org/6jmnvot2vmazsalriuoqhypmiteb7avnuzzk5abnpuou3ghgvhtvxayxnkyq/v.mp4"
+        //   // url="https://ajye.vizcloud.ink/simple/EqPFIfwQWADtjDlGha7rC5ouqVwVvra2AgR7rqk+wYMnU94US2El/br/list.m3u8"
+        // ></DuckiPlayer>
       )}
     </div>
   );
